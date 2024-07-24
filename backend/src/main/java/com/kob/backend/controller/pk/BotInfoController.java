02d7928a9498cca14fa0cabd8affb963f1cbbb18
godj <563030220@qq.com>
2024-07-24 17:49:53 +0800

    cdd

--- backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -1,7 +1,13 @@
 package com.kob.backend.controller.pk;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/pk/")
 public class BotInfoController {
+    @RequestMapping("getbotinfo/")
+    public String getBOtInfo(){
+        return "hhhhh";
+    }
 }
