622d7c8a847a331e8e9b89bfe29469189dcd8680
godj <563030220@qq.com>
2024-07-29 11:36:40 +0800

    7.28

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,33 +1,16 @@
 <template>
-  <div>
-    111
-    <div>Bot: {{ bot_name }}</div>
-    <div>战力：{{ bot_rating }}</div>
-  </div>
+  <NavBar/>
+
   <router-view/>
 </template>
-<script>
-import $ from 'jquery';
-import {ref} from 'vue';
-export default{
-  name:"App",
-  setup:() => {
-    let bot_name=ref("");
-    let bot_rating =ref("");
 
-    $.ajax({
-      url:"http://localhost:3000/pk/getbotinfo/",
-      type:"get",
-      success: resp => {
-        bot_name.value=resp.name;
-        bot_rating.value=resp.rating;
-        console.log(resp);
-      }
-    });    
-    return {
-      bot_name,
-      bot_rating
-    }
+<script>
+import NavBar from './components/NavBar.vue';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+export default{
+  components:{
+    NavBar
   }
 }
 
--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,7 +1,42 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import PkIndexView from '../views/pk/PkIndexView.vue'
+import RecordIndexView from '../views/record/RecordIndexView.vue'
+import RanklistIndexView from '../views/ranklist/RanklistIndexView.vue'
+import UserBotIndexView from '../views/user/bots/UserBotIndexView.vue'
+import NotFound from '../views/error/NotFound.vue'
+
 
 const routes = [
-
+  {
+    path:'/',
+    name: "home",
+    redirect: "/pk/",
+  },
+  {
+    path:"/pk/",
+    name:"pk_index",
+    component:PkIndexView,
+  },
+  {
+    path:"/record/",
+    name:"record_index",
+    component:RecordIndexView,
+  },
+  {
+    path:"/ranklist/",
+    name:"rank_index",
+    component:RanklistIndexView,
+  },
+  {
+    path:"/user/bot/",
+    name:"user_bot_index",
+    component:UserBotIndexView,
+  },
+  {
+    path:"/404/",
+    name:"404",
+    component:NotFound,
+  },
 ]
 
 const router = createRouter({
